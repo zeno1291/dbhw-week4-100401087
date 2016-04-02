3307eb7783fa8297bb29a6e3c2cd7896f5dc5a50
zeno <pongzeno199@gmail.com>
2016-04-02 23:01:06 +0800

    s

---
+++ main.js
@@ -0,0 +1,14 @@
+//var API_PATH = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=f4a75ba9-7721-4363-884d-c3820b0b917c";
+var API_PATH = "http://data.kaohsiung.gov.tw/Opendata/DownLoad.aspx?Type=2&CaseNo1=AV&CaseNo2=2&FileType=1&Lang=C&FolderType=";
+function getData(cb) {
+  $.ajax({
+    url : API_PATH,
+    method : "GET",
+    success : function(data) {
+      cb(null, data);
+    },
+    error : function(err) {
+      cb(err);
+    }
+  });
+}
